d2e134c1318ec786dfd9c12f25241d85e39ec3d3
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-23 11:15:18 +0300

    added route the website

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link> 
+    <router-link to="/about">About Me</router-link>
+    <router-link to="/skills">Skills</router-link> 
+    <router-link to="/work">Work</router-link>
+    <router-link to="/contact">Contact</router-link>
   </nav>
   <router-view/>
 </template>
@@ -11,20 +14,28 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
-
+*{
+  margin: 0;
+  padding: 0;
+}
 nav {
-  padding: 30px;
+  display: flex;
+  align-items: right;
+  background: linear-gradient(45deg, rgb(156, 14, 156), midnightblue);
+  padding: 15px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  font-size: 20px;
+  text-decoration: none;
+  color: white;
+  padding: 20px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: yellow;
 }
 </style>
