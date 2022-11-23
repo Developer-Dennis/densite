acae8217b199eb53d0e62a7b6fb205fda22e190f
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-23 14:14:27 +0300

    Added Projects page route

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> 
     <router-link to="/about">About Me</router-link>
     <router-link to="/skills">Skills</router-link> 
-    <router-link to="/work">Work</router-link>
+    <router-link to="/projects">Projects</router-link>
     <router-link to="/contact">Contact</router-link>
   </nav>
   <router-view/>
