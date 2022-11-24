79cd5a0d0ed7179a96650be70e66edfe6c72f662
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-24 08:28:20 +0300

    created and styled projects page

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -1,36 +1,33 @@
 <template>
-  <h2>My Projects</h2>
+  <h2 style="font-size:28px;padding:10px">My Projects</h2>
+  <div class="card">
+    <div id = "leftbox">
+      <img :src="image" alt="buyvitu" style="width:100%" />
+      <div class="container">
+        <a :href="buyvitu" style="text-decoration:none;color:black; font-size: 20px;">Buy Vitu</a> 
+        <p>Buy vitu is a web application which is used to sell second hand phones where a seller can post image,description,price,location and contact details and they buyer can check the phone if he/she likes it he/she can contact the seller. The languages i used are Express js,Ejs,Mysql</p> 
+    </div>  </div>
+  </div>
 
-<div class="card">
-  <div id = "leftbox">
-    <img :src="image" alt="buyvitu" style="width:100%" />
-    <div class="container">
-      <h4><b>Buy Vitu</b></h4> 
-      <p>Buy vitu is a web application which is used to sell second hand phones where a seller can post image,description,price,location and contact details and they buyer can check the phone if he/she likes it he/she can contact the seller. The languages i used are Express js,Ejs,Mysql</p> 
-  </div>  </div>
-</div>
-
-
-<div class="card">
-  <div id = "middlebox">
-    <img :src="image1" alt="buyvitu" style="width:100%" />
-    <div class="container">
-      <h4><b>Tyds</b></h4> 
-      <p>Tyds is a social media web application that allows users to create account,sign in and post tyds which is text. The languages i used are Express js,Ejs,Mysql</p> 
+  <div class="card">
+    <div id = "middlebox">
+      <img :src="image1" alt="Tyds" style="width:100%" />
+      <div class="container">
+        <a :href="Tyds" style="text-decoration:none;color:black;font-size: 20px;">Tidings</a>  
+        <p>Tyds is a social media web application that allows users to create account,sign in and post tyds which is text. The languages i used are Express js,Ejs,Mysql</p> 
+      </div>
     </div>
   </div>
-</div>
 
-
-<div class="card">
-  <div id = "rightbox">
-    <img :src="image2" alt="buyvitu" style="width:100%" />
-    <div class="container">
-      <h4><b>Notes App</b></h4> 
-      <p>Notes app is a web application that enable a user create an account sign in create a note edit,delete and its secure since you cannot see other users notes. The languages i used are Express js,Ejs,Mysql</p> 
+  <div class="card">
+    <div id = "rightbox">
+      <img :src="image2" alt="notesapp" style="width:100%" />
+      <div class="container">
+        <a :href="NotesApp" style="text-decoration:none;color:black;font-size: 20px;">NotesApp</a> 
+        <p>Notes app is a web application that enable a user create an account sign in create a note edit,delete and its secure since you cannot see other users notes. The languages i used are Express js,Ejs,Mysql</p> 
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -41,44 +38,40 @@ export default {
       image:  "./img/buyvitu.png",
       image1:  "./img/notesapp.jpg",
       image2:  "./img/tyds.jpg",
+      buyvitu: "https://github.com/Developer-Dennis/Buy-vitu",
+      Tyds: "https://github.com/Developer-Dennis/tidings",
+      NotesApp: "https://github.com/Developer-Dennis/notesApp"
     }
   }
 }
 </script>
 
 <style>
-    .card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  width: 40%;
-}
 
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-
-.container {
-  padding: 2px 16px;
+.container p{
+  font-size: 18px;
 }
 
 #leftbox {
   float:left;
-  width:250px;
-  height:400px;
-  margin-left: 10px;
+  width:400px;
+  height:160px;
+  padding-left: 20px;
+  margin-left: 30px;
 }
 #middlebox{
-  padding: 20px;
   float:left;
   width:400px;
   height:160px;
-  margin-left: 10px;
+  padding-left: 20px;
+  margin-left: 30px;
 }
 #rightbox{
   float:right;
   width:400px;
   height:160px;
-  margin-left: 10px;
+  padding-left: 20px;
+  margin-right: 30px;
 }
 
 </style>
