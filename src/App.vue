bfbebe6444ec1f6e9621327e957724c27708a169
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-24 08:25:50 +0300

    added projects route

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> 
+    <router-link to="/">Dennis Kemboi</router-link> 
     <router-link to="/about">About Me</router-link>
     <router-link to="/skills">Skills</router-link> 
     <router-link to="/projects">Projects</router-link>
