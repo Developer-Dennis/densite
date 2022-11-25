dfe2c1619a7b5eb99a849d35c5736b11ca772922
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-25 10:42:06 +0300

    inserted the footer

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -1,10 +1,10 @@
 <template>
-  <h2 style="font-size:28px;padding:10px">My Projects</h2>
+  <h2 style="font-size:28px;padding:10px;">My Projects</h2>
   <div class="card">
     <div id = "leftbox">
       <img :src="image" alt="buyvitu" style="width:100%" />
       <div class="container">
-        <a :href="buyvitu" style="text-decoration:none;color:black; font-size: 20px;">Buy Vitu</a> 
+        <a :href="buyvitu" style="text-decoration:none;color:black; font-size: 20px; color:#9c0e9c;">Buy Vitu</a> 
         <p>Buy vitu is a web application which is used to sell second hand phones where a seller can post image,description,price,location and contact details and they buyer can check the phone if he/she likes it he/she can contact the seller. The languages i used are Express js,Ejs,Mysql</p> 
     </div>  </div>
   </div>
@@ -13,7 +13,7 @@
     <div id = "middlebox">
       <img :src="image1" alt="Tyds" style="width:100%" />
       <div class="container">
-        <a :href="Tyds" style="text-decoration:none;color:black;font-size: 20px;">Tidings</a>  
+        <a :href="Tyds" style="text-decoration:none;color:#9c0e9c;;font-size: 20px;">Tidings</a>  
         <p>Tyds is a social media web application that allows users to create account,sign in and post tyds which is text. The languages i used are Express js,Ejs,Mysql</p> 
       </div>
     </div>
@@ -23,7 +23,7 @@
     <div id = "rightbox">
       <img :src="image2" alt="notesapp" style="width:100%" />
       <div class="container">
-        <a :href="NotesApp" style="text-decoration:none;color:black;font-size: 20px;">NotesApp</a> 
+        <a :href="NotesApp" style="text-decoration:none;color:black;font-size: 20px;color:#9c0e9c;">NotesApp</a> 
         <p>Notes app is a web application that enable a user create an account sign in create a note edit,delete and its secure since you cannot see other users notes. The languages i used are Express js,Ejs,Mysql</p> 
       </div>
     </div>
